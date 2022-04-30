9d1c9f83be36db5b3d1153af4fcc0fc380515ec6
nbrown225 <chbrown225@yahoo.com>
2022-04-30 05:14:31 -0700

    added sudo code to js

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var startBtn = document.querySelector("start-btn")
+var nextBtn = document.querySelector("next-btn")
+var questionBox = document.querySelector("question-box")
+// string of questions
+
+// string of correct answers
+
+// start quiz
+
+    // start timer
+    // show questions
+        // add questions to each option
+
+// choose question
+
+    // if correct, add points
+        // move to next question
+    
+    // if incorrect, remove points
+        // move to next question
+
+// show results
+    //show score
+    
+// times up
+    //show results
