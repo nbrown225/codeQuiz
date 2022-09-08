c640f41eccec1452203fcb43448684daa2ec5c70
nbrown225 <chbrown225@yahoo.com>
2022-09-07 19:28:54 -0700

    stopped timer from going below 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,10 +15,16 @@ var timeLeft = 10;
 // var questionNumber = 0;
 var questionCount = 1;
 function startTimer(){
-    setInterval(() => {
+    var time = setInterval(() => {
         timeLeft--;
         timerEl.textContent = 'TIME: ' + timeLeft;  
-          
+        
+        // stop quiz at 0
+        if (timeLeft <= 0) {
+            clearInterval(time)
+            timerEl.textContent = "OUT OF TIME!"
+            
+        }
     }, 1000);
 }
 function startQuiz() {
