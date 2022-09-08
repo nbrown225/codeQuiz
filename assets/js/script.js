7a3d906b1e37c23de6f0477cf1d774b05054aa2c
nbrown225 <chbrown225@yahoo.com>
2022-09-07 19:18:10 -0700

    added timer. showing on page

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// LINK DOM 
+    //- intro card
+var intro = document.querySelector('#intro');
+    
+    //- buttons
+var start = document.querySelector('#start');
+    
+    //- questions / answers
+var questionBox = document.querySelector('#question-box');
+var questionTitle = document.querySelector('#question-title');
+
+// ------------ TIMER -----------------------
+var timerEl = document.querySelector('#timer');
+var timeLeft = 10;
+// var questionNumber = 0;
+var questionCount = 1;
+function startTimer(){
+    setInterval(() => {
+        timeLeft--;
+        timerEl.textContent = 'TIME: ' + timeLeft;  
+          
+    }, 1000);
+}
+function startQuiz() {
+    startTimer();
+}
+
+start.addEventListener('click', startQuiz)
